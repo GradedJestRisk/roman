e0be1da6966fd76ac1e390f6dded851f9ca0d851
GradedJestRisk <graded.jest.risk@gmail.com>
2023-10-11 16:41:04 +0200

    Add nested tests

--- app/src/test/java/roman/RomanTest.java
+++ app/src/test/java/roman/RomanTest.java
@@ -1,27 +1,38 @@
 package roman;
 
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class RomanTest {
-    @Test void given1ShouldReturnI() {
-        // given
-        Integer i = 1;
+    @Nested
+    class whenGiven1 {
+        @Test
+        void shouldReturnI() {
+            // given
+            Integer i = 1;
 
-        // when
-        String value = Roman.decimalToRoman(i);
+            // when
+            String value = Roman.decimalToRoman(i);
 
-        // then
-        assertThat(value).isEqualTo("I");
+            // then
+            assertThat(value).isEqualTo("I");
+        }
     }
-    @Test void given5ShouldReturnV() {
-        // given
-        Integer i = 5;
 
-        // when
-        String value = Roman.decimalToRoman(i);
+    @Nested
+    class whenGivenV {
+        @Test void shouldReturnV() {
+            // given
+            Integer i = 5;
 
-        // then
-        assertThat(value).isEqualTo("V");
+            // when
+            String value = Roman.decimalToRoman(i);
+
+            // then
+            assertThat(value).isEqualTo("V");
+        }
+
     }
 }
